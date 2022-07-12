f0310413b9908bb24a055ea8b8d234d76950e631
Miguel <miguel.rosas@ua.pt>
2022-07-12 01:43:05 +0100

    Update á macro do histograma temporal do número de hits

--- Histogramas_DT_H_Detetor.C
+++ Histogramas_DT_H_Detetor.C
@@ -1,42 +1,63 @@
-void Histogramas_DT_H_Detetor(TString ficheiroLido){ 
+void Histogramas_DT_H_Detetor(){ 
+
+// Seleção dos ficheiro de dados
+TFile *f = new TFile("AmberTarget_Run_1.root","READ");
 
 
-//Seleção de ficheiros a ler assim como escrever
-TFile *ficheiro = new TFile("AmberTarget_Run_0.root","READ");
-	
-TString novoFicheiro=ficheiroLido;
-novoFicheiro.ReplaceAll("AmberTarget_Run_","Analise_Histogramas_DT_H_Detetor_");
-	
-TFile *ficheiroGravar = new TFile(novoFicheiro,"RECREATE");
-TTree *dados = (TTree*) ficheiro->Get("Hits");/*Seleção da árvore Hits em que iremos buscar a branch particleHitTime, para obter o tempo que as particulas demoraram para chegar ao detetor e selecionamos também a branch detectorID para filtrar as o tempo de choque das particulas para os respetivos detetores*/
-	 
-Int_t nBins = 50;
-Double_t minBin = 0.0;
-Double_t maxBin = 400;
-	
+// Seleção da árvore onde estão os hits temporais
+TTree *dados = (TTree*)f->Get("Hits");
+
 Int_t nHistos = 4;
-TH1D* histo[nHistos];
-TString branchName;
+TH1F *histoDetetor [nHistos];
+
+Int_t nBins=500;
+Double_t minBin=0;
+Double_t maxBin=10;
+Double_t particleTime;
+Int_t detetorID;
+
+// iremos "desativar" as branches, para mais tarde selecionar apenas as que precisamos por questões de otimização
+dados->SetBranchStatus("*",0);
+Long64_t Nentries = dados->GetEntries();
 
 
-TCanvas *canvas = new TCanvas("canvas", novoFicheiro, 900, 700);
-gStyle->SetOptStat(0);
-canvas->Divide(2,2,0,0);
+// criação dos histogramas que irão guardas os hits de cada detetor
+TString detetorTitle;
+detetorTitle="Detector 0";
+histoDetetor[0] = new TH1F(detetorTitle,detetorTitle,nBins,minBin,maxBin);
+detetorTitle="Detector 1";
+histoDetetor[1] = new TH1F(detetorTitle,detetorTitle,nBins,minBin,maxBin);
+detetorTitle="Detector 2";
+histoDetetor[2] = new TH1F(detetorTitle,detetorTitle,nBins,minBin,maxBin);
+detetorTitle="Detector 3";
+histoDetetor[3] = new TH1F(detetorTitle,detetorTitle,nBins,minBin,maxBin);
 
-	
-/*A cada iteração iremos desenhar um histograma para um dado detetor, para isto iremos filtrar os dados de particleHitTime usando o detectorID que igual ao foi selecionado durante a iteração*/
-for(Int_t i = 0 ;i < nHistos; i++){
 
-	TString HistoNome = "Histograma " + TString::Itoa(i+1, 10);
-	histo[i] = new TH1D(HistoNome, HistoNome, nBins, minBin, maxBin);
-	branchName = "particleHitTime_ns";
-   	canvas->cd(i+1);
-	dados->Draw(branchName + ">>" + HistoNome, "detectorID=="+ TString::Itoa(i, 10) );//Seleção dos dados e a condição de o detectorID ser o que foi selecionado neesta iteração
-	gPad->SetLogy(); //selecionar escala logaritmica
-	histo[i]->SetFillColor(i+1);
-   	histo[i]->SetTitle(HistoNome);
-	histo[i]->Write();
+// seleção das branches de dados
+dados->SetBranchStatus("detectorID",1);
+dados->SetBranchStatus("particleHitTime_ns",1);
+dados->SetBranchAddress("detectorID",&detetorID);
+dados->SetBranchAddress("particleHitTime_ns",&particleTime);
 
-	}
+// itereção pelas entradas das branches
+for (Int_t i = 0; i < Nentries; i++)
+{
+	dados->GetEntry(i);
+	histoDetetor[detetorID]->Fill(particleTime); // com o detetorID obtemos o ID de detetor e adicionamos o tempo da particula
+}
+// criação da stack de histogramas para agrupar os histogramas dos detetores no mesmo grafico
+THStack *hs = new THStack("hs","Hits por tempo nos Detetores; tempo (ns); Quantidade de Hits");
+TCanvas *cs = new TCanvas("Hits por tempo dos detetores","Hits por tempo dos detetores",10,10,700,500);
+TText T; T.SetTextFont(42); T.SetTextAlign(21);
+
+// adição dos histogramas á stack
+for (Int_t i = 0; i < nHistos;i++)
+{
+	histoDetetor[i]->SetFillColor(i+1);
+	histoDetetor[i]->SetLineColor(i+1);
+	hs->Add(histoDetetor[i]);
+}
+hs->Draw("noclear");
+cs->BuildLegend();
 
 }
